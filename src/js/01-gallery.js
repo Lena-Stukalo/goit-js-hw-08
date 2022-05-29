795109d491b516507b827a2e753cb7fa3a10d9ac
Lena-Stukalo <68201887+Lena-Stukalo@users.noreply.github.com>
2022-05-29 16:20:39 +0300

    1,2 task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,20 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox';
 import { galleryItems } from './gallery-items';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
+const gallery = document.querySelector('.gallery');
 
-console.log(galleryItems);
+const galleryList = galleryItems
+  .map(({ original, preview, description }) => {
+    return `<div><a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a></div>`;
+  })
+  .join(' ');
+gallery.innerHTML = galleryList;
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,37 @@
+import vimeoPlayer from '@vimeo/player';
+var throttle = require('lodash.throttle');
 
+const STORAGE_KEY = 'videoplayer-current-time';
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+player.on('timeupdate', throttle(onPlayTimeSet, 1000));
+
+onPlayTimeGet();
+
+function onPlayTimeSet(e) {
+  localStorage.setItem(STORAGE_KEY, e.seconds);
+}
+
+function onPlayTimeGet() {
+  const currentSecond = localStorage.getItem(STORAGE_KEY);
+  if (currentSecond) {
+    player
+      .setCurrentTime(currentSecond)
+      .then(function (seconds) {
+        // seconds = the actual time that the player seeked to
+      })
+      .catch(function (error) {
+        switch (error.name) {
+          case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+          default:
+            // some other error occurred
+            break;
+        }
+      });
+  }
+}
